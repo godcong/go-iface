f3ed2184d49f62d574f991a2bcbeeb0d34d0b399
godcong <jumbycc@163.com>
2023-04-04 23:04:49 +0800

    feat(generate): go interface generate

--- test/test_gen_map.go
+++ test/test_gen_map.go
@@ -7,6 +7,6 @@ func (mapGen) MapRet() map[string]map[string]int {
 	return nil
 }
 
-func (mapGen) MapIn(a, b map[int]map[string]FuncMethod) {
+func (mapGen) MapIn(a, b map[int]map[string]any) {
 
 }
